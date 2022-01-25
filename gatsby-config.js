a5857bf8c2048d026abe74a8ff894a2903851e44
GabrielBarros <gabriel.barros@vtex.com.br>
2022-01-25 16:03:52 -0300

    Static Images and GraphQL Queries Added

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,17 @@
 module.exports = {
     siteMetadata: {
-      title: `GastbyTutorial`,
+      title: `Gastby Tutorial`,
         siteUrl: `https://www.yourdomain.tld`,
     },
     plugins: [
-
+      `gatsby-plugin-image`,
+      `gatsby-plugin-sharp`,
+      {
+        resolve: "gatsby-source-filesystem",
+        options: {
+          name: `blog`,
+          path: `${__dirname}/blog`,
+        }
+      },
     ]
 }
